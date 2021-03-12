768f3df91006fe269305496fab143f9b7b3139b5
major1201 <major1201@gmail.com>
2021-03-12 22:00:30 +0800

    feat: support more types functions

--- types.go
+++ types.go
@@ -1,6 +1,10 @@
 package goutils
 
-import "reflect"
+import (
+	"context"
+	"reflect"
+	"unsafe"
+)
 
 var (
 	reflectTypeSetForNilValidation = func() map[reflect.Kind]struct{} {
@@ -39,4 +43,99 @@ func IsNil(object interface{}) bool {
 	}
 	// object has type, so we check its value
 	return IsValueNil(reflect.ValueOf(object))
+}
+
+// IsTypeFunction returns if the argument is reflect.Func.
+func IsTypeFunction(typ reflect.Type) bool {
+	return typ.Kind() == reflect.Func
+}
+
+// IsTypeIteratee returns if the argument is an iteratee.
+func IsTypeIteratee(typ reflect.Type) bool {
+	kind := typ.Kind()
+	return kind == reflect.Array || kind == reflect.Slice || kind == reflect.Map
+}
+
+// IsTypeCollection returns if the argument is a slice/array.
+func IsTypeCollection(typ reflect.Type) bool {
+	kind := typ.Kind()
+	return kind == reflect.Array || kind == reflect.Slice
+}
+
+// IsTypeSlice returns if argument is slice type
+func IsTypeSlice(typ reflect.Type) bool {
+	return typ.Kind() == reflect.Slice
+}
+
+// IsTypeArray returns if argument is array type
+func IsTypeArray(typ reflect.Type) bool {
+	return typ.Kind() == reflect.Array
+}
+
+// IsTypeMap returns if the argument is a map
+func IsTypeMap(typ reflect.Type) bool {
+	return typ.Kind() == reflect.Map
+}
+
+var errorType = reflect.TypeOf((*error)(nil)).Elem()
+
+// IsTypeImplementsError returns if the argument is impl of error
+func IsTypeImplementsError(typ reflect.Type) bool {
+	return typ.Implements(errorType)
+}
+
+var ctxType = reflect.TypeOf((*context.Context)(nil)).Elem()
+
+// IsTypeImplementsContext returns if the argument is impl of context.Context
+func IsTypeImplementsContext(typ reflect.Type) bool {
+	return typ.Implements(ctxType)
+}
+
+// ValuesToPtrs transfer []Type to []*Type.
+// Panics When:
+//  1. s is not slice (array is not allowed);
+//  2. elem is pointer type;
+//  3. elem is unaddressable value
+func ValuesToPtrs(s interface{}) interface{} {
+	sVal := reflect.ValueOf(s)
+	sKind := sVal.Kind()
+	if sKind != reflect.Slice {
+		panic("s is not slice")
+	}
+
+	elemType := sVal.Type().Elem()
+	if elemType.Kind() == reflect.Ptr {
+		panic("elem is pointer type")
+	}
+
+	sLen := sVal.Len()
+	rType := reflect.SliceOf(reflect.PtrTo(elemType))
+	if sLen == 0 {
+		return reflect.Zero(rType).Interface()
+	}
+
+	result := reflect.MakeSlice(rType, 0, sLen)
+	for index := 0; index < sLen; index++ {
+		result = reflect.Append(result, sVal.Index(index).Addr())
+	}
+	return result.Interface()
+}
+
+// UnsafeBytesToStr returns the byte slice as a volatile string
+// THIS IS EVIL CODE.
+// YOU HAVE BEEN WARNED.
+func UnsafeBytesToStr(b []byte) string {
+	// same as strings.Builder::String()
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// UnsafeStrToBytes returns the string as a byte slice
+// THIS IS EVIL CODE.
+// YOU HAVE BEEN WARNED.
+func UnsafeStrToBytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Len:  len(s),
+		Cap:  len(s),
+		Data: (*reflect.StringHeader)(unsafe.Pointer(&s)).Data,
+	}))
 }
--- types_test.go
+++ types_test.go
@@ -1,8 +1,10 @@
 package goutils
 
 import (
+	"context"
 	"reflect"
 	"testing"
+	"time"
 	"unsafe"
 
 	"github.com/stretchr/testify/assert"
@@ -92,4 +94,181 @@ func TestIsNil(t *testing.T) {
 	// unsafe.Pointer
 	up := unsafe.Pointer(nil)
 	assert.True(t, IsNil(up))
+}
+
+type tmpCtx int
+
+func (*tmpCtx) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (*tmpCtx) Done() <-chan struct{} {
+	return nil
+}
+
+func (*tmpCtx) Err() error {
+	return nil
+}
+
+func (*tmpCtx) Value(key interface{}) interface{} {
+	return nil
+}
+
+func (e *tmpCtx) String() string {
+	return "tmpCtx"
+}
+
+type tmpStruct struct {
+	msg string
+}
+
+type tmpError struct {
+	msg string
+}
+
+func (e *tmpError) Error() string {
+	return e.msg
+}
+
+func TestUnsafe(t *testing.T) {
+	is := assert.New(t)
+
+	ss := []string{"a", "b", "c"}
+	mp := map[int]int{1: 2, 3: 4}
+
+	var mpNil map[int]int
+	is.PanicsWithValue("value is invalid", func() { IsValueNil(reflect.ValueOf(nil)) })
+	is.True(IsValueNil(reflect.ValueOf(mpNil)))
+	is.False(IsValueNil(reflect.ValueOf(123)))
+
+	fn := func(a, b int) int {
+		return a + b
+	}
+	is.True(IsTypeFunction(reflect.TypeOf(fn)))
+	is.False(IsTypeFunction(reflect.TypeOf(mpNil)))
+	is.False(IsTypeFunction(reflect.TypeOf(ss)))
+
+	is.True(IsTypeCollection(reflect.TypeOf(ss)))
+	is.False(IsTypeCollection(reflect.TypeOf(mp)))
+
+	is.True(IsTypeMap(reflect.TypeOf(mp)))
+	is.False(IsTypeMap(reflect.TypeOf(ss)))
+
+	is.True(IsTypeImplementsError(reflect.TypeOf(&tmpError{"hello"})))
+	is.False(IsTypeImplementsError(reflect.TypeOf(tmpError{"hello"})))
+	is.False(IsTypeImplementsError(reflect.TypeOf(&tmpStruct{"hello"})))
+	is.False(IsTypeImplementsError(reflect.TypeOf(tmpStruct{"hello"})))
+
+	ctx := context.Background()
+	is.True(IsTypeImplementsContext(reflect.TypeOf(ctx)))
+
+	tCtx := new(tmpCtx)
+	is.True(IsTypeImplementsContext(reflect.TypeOf(tCtx)))
+	is.True(IsTypeImplementsContext(reflect.TypeOf(ctx)))
+	is.False(IsTypeImplementsContext(reflect.TypeOf(&tmpStruct{"hello"})))
+	is.False(IsTypeImplementsContext(reflect.TypeOf(tmpStruct{"hello"})))
+}
+
+type testStruct struct {
+	A string
+}
+
+func TestValuesToPtrs(t *testing.T) {
+	is := assert.New(t)
+
+	is.PanicsWithValue(
+		"s is not slice",
+		func() {
+			ValuesToPtrs(1)
+		},
+	)
+	is.PanicsWithValue(
+		"elem is pointer type",
+		func() {
+			modelPtrs := []*testStruct{{"1"}, {"2"}}
+			ValuesToPtrs(modelPtrs)
+		},
+	)
+
+	intPtrs := ValuesToPtrs([]int{}).([]*int)
+	is.Empty(intPtrs)
+
+	intVals := []int{1, 2, 3, 4, 5}
+	intPtrs = ValuesToPtrs([]int{1, 2, 3, 4, 5}).([]*int)
+	for index, intPtr := range intPtrs {
+		is.Equal(intVals[index], *intPtr)
+	}
+
+	models := []testStruct{
+		{"1"},
+		{"2"},
+		{"3"},
+	}
+	modelPtrs := ValuesToPtrs(models).([]*testStruct)
+	for index, modelPtr := range modelPtrs {
+		is.Equal(models[index], *modelPtr)
+	}
+}
+
+var (
+	testStr   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	testBytes = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+func TestUnsafeBytesToStr(t *testing.T) {
+	s := UnsafeBytesToStr(testBytes)
+	assert.Equal(t, testStr, s)
+}
+
+func TestUnsafeStrToBytes(t *testing.T) {
+	bs := UnsafeStrToBytes(testStr)
+	assert.Equal(t, true, reflect.DeepEqual(bs, testBytes))
+}
+
+func BenchmarkBytesToStr(b *testing.B) {
+	b.Run(
+		"BytesToStr-UnSafe",
+		func(b *testing.B) {
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				_ = UnsafeBytesToStr(testBytes)
+			}
+		},
+	)
+
+	b.Run(
+		"BytesToStr-Safe",
+		func(b *testing.B) {
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				_ = string(testBytes)
+			}
+		},
+	)
+}
+
+func BenchmarkStrToBytes(b *testing.B) {
+	b.Run(
+		"StrToBytes-UnSafe",
+		func(b *testing.B) {
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				_ = UnsafeStrToBytes(testStr)
+			}
+		},
+	)
+
+	b.Run(
+		"StrToBytes-Safe",
+		func(b *testing.B) {
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				_ = []byte(testStr)
+			}
+		},
+	)
 }
